495ec942e657a9406b25723c53d050db6afdd75f
cycyjing <cycycyjing@gmail.com>
2023-12-08 11:11:51 +1300

    feat: Create Product List component

---
+++ src/components/product-list/ProductList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ProductList = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Product Title</th>
+          <th>Source</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+    </table>
+  )
+}
+
+export default ProductList
