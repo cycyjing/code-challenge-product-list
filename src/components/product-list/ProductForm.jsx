6f6c40ce4393f65f4ff497c21371b7e8d4efb099
cycyjing <cycycyjing@gmail.com>
2023-12-08 11:26:59 +1300

    feat: Get product form value

--- src/components/product-list/ProductForm.jsx
+++ src/components/product-list/ProductForm.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ProductForm = () => {
+  const [product, setProduct] = useState({
+    title: "",
+    source: "",
+  });
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const handleSubmit = () => {
+    console.log("product", product);
+    setProduct({
+      title: "",
+      source: "",
+    });
+    setIsDisabled(true);
+  };
+
   return (
     <table>
       <tbody>
@@ -9,7 +24,18 @@ const ProductForm = () => {
             <label htmlFor="title">Product Title:</label>
           </td>
           <td>
-            <input type="text" name="title" id="title" />
+            <input
+              type="text"
+              name="title"
+              id="title"
+              value={product.title}
+              onChange={(e) => {
+                setProduct((pre) => {
+                  return { ...pre, title: e.target.value };
+                });
+                setIsDisabled(e.target.value.length === 0);
+              }}
+            />
           </td>
         </tr>
         <tr>
@@ -17,7 +43,16 @@ const ProductForm = () => {
             <label htmlFor="source">Source:</label>
           </td>
           <td>
-            <select name="source" id="source">
+            <select
+              name="source"
+              id="source"
+              value={product.source}
+              onChange={(e) => {
+                setProduct((pre) => {
+                  return { ...pre, source: e.target.value };
+                });
+              }}
+            >
               <option value="">Select a source</option>
               <option value="YouTube">YouTube</option>
               <option value="Google">Google</option>
@@ -27,7 +62,9 @@ const ProductForm = () => {
         </tr>
         <tr>
           <td colSpan={2}>
-            <button>Submit</button>
+            <button disabled={isDisabled} onClick={handleSubmit}>
+              Submit
+            </button>
           </td>
         </tr>
       </tbody>
